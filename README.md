4a263abda234b4a6c261588162c394f1381b2f0d
kikiru328 <kikiru328@gmail.com>
2022-07-21 12:00:28 +0900

    program

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# auto_database
+        Must private : Personal Informagtion 
+
+# Auto Formatting order table
+
+> two Order Table merge to same format DataFram
+
+#### Functions
+
+- Functions are operated by Pandas
+- python 3.8.13
+
+#### GUI
+
+- PyQt6 
+- UI : QtDesigner 
--- program/pyqt6.py
+++ program/pyqt6.py
@@ -1,22 +1,50 @@
 import sys
 from PyQt6.QtWidgets import *
-from PyQt6.QtGui import *  # 아이콘 객체
+from PyQt6 import uic
+import os
 
-app = QApplication(sys.argv) # 필수객체
+UI_class = uic.loadUiType('uniform.ui')[0]
 
-class MyWindow(QMainWindow): # 윈도우 객체
+class main_window(QMainWindow, UI_class):
     def __init__(self):
         super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle('자사몰 네이버 포맷')
+    
+        self.ImWeb_file.setText('자사몰')
+        self.Naver_file.setText('스마트스토어')
+        self.original_label.setText('오리지널 저장 경로')
+        self.honest_label.setText('어니스트 저장 경로')
+    
+        self.ImWeb_file_btn.clicked.connect(self.imweb_FileLoad)
+        self.Naver_file_btn.clicked.connect(self.naver_FileLoad)  
+        self.Format_func_btn.clicked.connect(self.Format_functions)
         
-        self.setWindowTitle('ex')  # 윈도우 창 타이틀
+    def imweb_FileLoad(self):
+        global iwfname
+        iwfname = QFileDialog.getOpenFileName(self)
+        self.ImWeb_file.setText(os.path.basename(iwfname[0]))
         
-        self.setGeometry(300,300,400,400) # 창 위치 x,y / 창 크기 x,y
+    
+    
+    def naver_FileLoad(self):
+        global nvfname
+        nvfname = QFileDialog.getOpenFileName(self)
+        self.Naver_file.setText(os.path.basename(nvfname[0]))
         
-        # self.setWindowIcon(QIcon('path')) # 아이콘 적용
-
-# window = QWidget() # 화면 창
-window = MyWindow()
-
-window.show() # 출력 
-
-app.exec_() # 닫기 전 까지 loop 
+        
+    
+    def Format_functions(self):
+        print(iwfname[0])
+        print(nvfname[0])
+        
+        
+    
+            
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    Window = main_window()
+    Window.show()
+    app.exec()
+        
